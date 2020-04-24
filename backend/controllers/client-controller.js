8ff7d39bacfce3d340ca49882a40236aa380a534
Federico <bertonefederico1@gmail.com>
2020-04-24 18:22:53 -0300

    correcion metodo getOne

--- backend/controllers/client-controller.js
+++ backend/controllers/client-controller.js
@@ -3,17 +3,47 @@
 const Client = require('../models/client-model');
 const clientController = { };
 
-clientController.getAll = ()=>{
-    Client.findAll({
+clientController.getAll = async (req, res) => {
+    await Client.findAll({
         attributes: ['id_cliente', 'dni', 'nombre', 'apellido', 'direccion', 'telefono']
     })
         .then( (clients) => {
-            console.log(clients);
+            res.json(clients);
         })
         .catch ((err) => {
             console.log(err);
         })
 }
+
+clientController.getOne = async (req, res) => {
+    await Client.findByPk(req.params.id, {
+        attributes: ['id_cliente', 'dni', 'nombre', 'apellido', 'direccion', 'telefono']
+    })
+        .then( (clients) => {
+            res.json(clients);
+        })
+        .catch ((err) => {
+            console.log(err);
+        })
+}
+
+/* clientController.createClient = async (req, res) => {
+    await Client.findOrCreate({
+        attributes: ['id_cliente', 'dni', 'nombre', 'apellido', 'direccion', 'telefono'],
+        where: {
+            dni: req.body.dni
+        },
+        defaults: {
+            dni: req.body.dni,
+            nombre: req.body.nombre,
+            apellido: req.body.apellido,
+            direccion: req.body.direccion,
+            telefono: req.body.telefono
+        }
+    })
+        .then(console.log('Client saved'))
+        .catch(console.log('Error'));   
+} */
 
 
 module.exports = clientController;
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -4,8 +4,9 @@ const express = require('express');
 const router = express.Router();
 const clientController = require('../controllers/client-controller');
 
+
 router.get('/clients', clientController.getAll); 
-
-
+router.get('/clients/:id', clientController.getOne);
+router.post('/addclient', clientController.createClient);
 
 module.exports = router;
