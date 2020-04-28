a7add3438c17ea6894fb7f1384901cb406ffabc9
Alejo Cuello <alejocuello1998@gmail.com>
2020-04-28 13:16:12 -0300

    Fix routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -8,19 +8,16 @@ const articleController = require('../controllers/article-controller');
 //Rutas de clientes
 router.get('/clients', clientController.getAll); 
 router.get('/clients/:id', clientController.getOne);
-<<<<<<< HEAD
 router.post('/addclient', clientController.createClient);
+router.put('/clients/:id', clientController.updateClient);
+router.delete('/deleteClient/:id', clientController.deleteClient);
 
 //Rutas de proveedores
 router.get('/providers', providerController.getAll); 
 router.get('/providers/:id', providerController.getOne);
 router.post('/addprovider', providerController.createClient);
-=======
-router.post('/addClient', clientController.createClient);
-router.put('/clients/:id', clientController.updateClient);
-router.delete('/deleteClient/:id', clientController.deleteClient);
 
+//Rutas de articulos
 router.get('articles', articleController.getAll);
->>>>>>> 8d479ae57e30266649b5f675e2f0a891fc515f8d
 
 module.exports = router;
