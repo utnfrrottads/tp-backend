e536b0349e28ca4b4c58ada20a1cebc2e41276ea
Federico <bertonefederico1@gmail.com>
2020-04-29 17:32:55 -0300

    intentando hacer join

--- backend/models/supplier-article-model.js
+++ backend/models/supplier-article-model.js
@@ -2,18 +2,20 @@
 
 const sequelize = require('../database/db-connection');
 const Sequelize = require('sequelize');
+const Article = require('./article-model');
+const Supplier = require('./supplier-model');
 
 const Supplier_Article = sequelize.define('proveedores_articulos', {
     id_articulo: { type: Sequelize.INTEGER, 
                    references: { 
-                       model: 'articulos', 
+                       model: Article, 
                        key: 'id_articulo'
                     }, 
                     primaryKey: true
                 },
     id_proveedor: { type: Sequelize.INTEGER,
                     references: {
-                        model: 'proveedores',
+                        model: Supplier,
                         key: 'id_proveedor'
                     },
                     primaryKey: true 
