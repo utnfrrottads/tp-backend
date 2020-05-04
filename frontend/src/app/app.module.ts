f18cd711b71e89c1f6b8572c479bbaaae1624b63
Federico <bertonefederico1@gmail.com>
2020-05-04 20:25:35 -0300

    comienzo frontend

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ClientComponent } from './components/client/client.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ClientComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
