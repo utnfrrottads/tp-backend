420029073c0076da9b600ac6b15e376a70aa100b
Matias Peralta <matias_peralta1@hotmail.com>
2020-06-17 11:34:33 -0300

    ABMC Branch

--- server/routes/branch.routes.js
+++ server/routes/branch.routes.js
@@ -1,1 +1,11 @@
-//Rutas aqui
+const express = require('express');
+const branchRouter = express.Router();
+const branchCtrl = require('../controllers/branch.controller');
+
+branchRouter.get('/', branchCtrl.getBranches);
+branchRouter.get('/:id', branchCtrl.getBranch);
+branchRouter.post('/', branchCtrl.createBranch);
+branchRouter.put('/:id', branchCtrl.editBranch);
+branchRouter.delete('/:id', branchCtrl.deleteBranch);
+
+module.exports = branchRouter;
