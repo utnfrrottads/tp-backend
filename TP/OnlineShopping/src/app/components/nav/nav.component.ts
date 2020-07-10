3692d81df0a74e9d28093ec87351c206bedca348
Danilo Bassi <danilo-bassi@hotmail.com>
2020-07-10 11:09:53 -0300

    Responsive agregado

--- TP/OnlineShopping/src/app/components/nav/nav.component.ts
+++ TP/OnlineShopping/src/app/components/nav/nav.component.ts
@@ -1,32 +1,46 @@
-import { Component, OnInit, Input } from '@angular/core';
-import {Producto} from '../../model/productos'
-declare var M:any;
+import { Component, OnInit, Input, HostListener } from '@angular/core';
+import { Producto } from '../../model/productos';
+declare var M: any;
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
-
-  
-  elems:any;
-  instances:any;
+  elems: any;
+  instances: any;
   options = [];
+  nav_string = '';
 
-  @Input() lista:Producto[];
+  @Input() lista: Producto[];
 
+  constructor() {}
 
-  constructor() { }
-  
+  @HostListener('window:resize', [])
+  public onResize() {
+    this.detectScreenSize();
+  }
+  detectScreenSize() {
+    const width = window.innerWidth;
+    if(width<=380){
+      this.nav_string = "Buscar"
+    }
+    else{
+      this.nav_string = '¡Buscar un producto!';
+    }
+  }
+
   ngOnInit(): void {
+    this.detectScreenSize();
     this.elems = document.querySelectorAll('.dropdown-trigger');
     this.instances = M.Dropdown.init(this.elems, this.options);
   }
-  search(input)
-  {
-    console.log("Se ha buscado un producto usando la palabra:" + input.value);
-    input.value = "";
+  search(input) {
+    let texto = input.value.trim();
+    if (texto !== '') {
+      console.log('Se ha buscado un producto usando la palabra:' + texto);
+      input.value = '';
+    }
   }
-
 }
