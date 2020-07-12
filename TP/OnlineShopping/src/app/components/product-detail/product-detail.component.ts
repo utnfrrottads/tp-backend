51da2073074c61a10730973aec4287d817adcb07
campi97 <53315236+campi97@users.noreply.github.com>
2020-07-12 02:56:49 -0300

    product-detail first version

--- TP/OnlineShopping/src/app/components/product-detail/product-detail.component.ts
+++ TP/OnlineShopping/src/app/components/product-detail/product-detail.component.ts
@@ -8,9 +8,13 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ProductDetailComponent implements OnInit {
 @Input() producto: any;
 
-  constructor() { }
+  constructor() { 
+    this.producto= {idProducto: 1, idRubro: 1,  idEmpresa: 1, nombre: "Notebook", imagen: "https://http2.mlstatic.com/notebook-lenovo-i3-8130u-4gb-1tb-156-pulgadas-dvdrw-D_NQ_NP_872956-MLA42418883269_062020-F.webp", precio: 50000,  descripcion: "Es una notebook, un producto muy bueno y de alta calidad.", stock: 25 };
+  }
 
   ngOnInit(): void {
   }
+
+
 
 }
