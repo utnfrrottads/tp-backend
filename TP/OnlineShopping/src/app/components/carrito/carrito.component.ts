cac40653d59851ae277c736e57b5b136016abfd0
Danilo Bassi <danilo-bassi@hotmail.com>
2020-07-14 18:54:37 -0300

    Agregado de carrito. Faltan los botones de comprar definitivos

--- TP/OnlineShopping/src/app/components/carrito/carrito.component.ts
+++ TP/OnlineShopping/src/app/components/carrito/carrito.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-
-
 @Component({
   selector: 'app-carrito',
   templateUrl: './carrito.component.html',
@@ -12,9 +10,7 @@ export class CarritoComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    //creo una lista para probar.
     this.list = [
-      
       {
         idProducto: 3,
         idRubro: 2,
@@ -36,11 +32,11 @@ export class CarritoComponent implements OnInit {
           'https://cnet1.cbsistatic.com/img/_RKt08nA2AkU4hgTJHtoOwsuKAI=/980x0/2020/05/02/f7bad802-c8c5-4a3d-8977-d5848a515c48/galaxy-a51.jpg',
         precio: 32199,
         descripcion:
-          'Rapido, caro. Mejor que un Iphone. voy a usar esto para rellenar con un texto largo para ver si funciona esto de truncar el texto a partir de la 3ra linea.. asi que voy a rellenar con la cancion de marolio, le da sabor a tu vida, marolio',
+          'Telefono completo. Viene con funda de platino, bañada con diamantes sacados del machupichu/peru. Lo sacamos a la fuerza entrando con una escavadora y atropellando 30 peruanos. El gerente termino preso ',
         stock: 3,
       },
       {
-        idProducto: 2,
+        idProducto: 4,
         idRubro: 2,
         idEmpresa: 1,
         nombre: 'Bicicleta para hacer Modelos del dominio',
@@ -51,7 +47,47 @@ export class CarritoComponent implements OnInit {
           'Bicicleta para tomar examen en la UTN FRRo: Materia ADES. Caes con esta al aula y decis que sos la profesora',
         stock: 3,
       },
+      {
+        idProducto: 5,
+        idRubro: 2,
+        idEmpresa: 1,
+        nombre: 'Procesador Ryzen 7 1800x',
+        imagen:
+          'https://http2.mlstatic.com/mountain-bike-top-mega-rodado26-modelo-rowen-envios-gratis-D_NQ_NP_947011-MLA40463784361_012020-F.webp',
+        precio: 17888,
+          descripcion: 'Una bestia, lo tengo overclockeado a 3.9 GHZ, podria un poco mas, pero se me calienta demasiado jajaj salu2. En el red dead redemption 2 no llega ni al 40% del uso, en parte porque es potente, y otra parte porque esta optimizado como el orto. Eso si, la tarjeta de video me explota. Un saludo',
+        stock: 3,
+      },
     ];
-    // copie y pegue la del campi
+
+    //a cada elemento de la lista le agrego una cantidad para comprar
+    //por defecto será 1.
+    this.list.forEach((element) => {
+      element.cantComprar = 1;
+    });
+  }
+
+  add(producto) {
+    if (producto.cantComprar < producto.stock) {
+      producto.cantComprar++;
+    }
+  }
+  remove(producto) {
+    if (producto.cantComprar > 1) {
+      producto.cantComprar--;
+    }
+  }
+  delete(producto) {
+    const index = this.list.indexOf(producto);
+    if (index > -1) {
+      this.list.splice(index, 1);
+    }
+  }
+  precioFinal() {
+    let total = 0;
+    this.list.forEach((element) => {
+      total += element.cantComprar * element.precio;
+    });
+    return total;
   }
 }
