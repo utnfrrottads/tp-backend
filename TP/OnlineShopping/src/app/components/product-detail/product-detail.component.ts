abfb548e519a673dd054388e917828c7252eb86a
Gabriel Campitelli <53315236+campi97@users.noreply.github.com>
2020-07-14 21:19:25 -0300

    Arreglo product-detail responsive

--- TP/OnlineShopping/src/app/components/product-detail/product-detail.component.ts
+++ TP/OnlineShopping/src/app/components/product-detail/product-detail.component.ts
@@ -11,7 +11,7 @@ export class ProductDetailComponent implements OnInit {
 
   constructor(private route:ActivatedRoute) { 
     this.producto= {idProducto: 1, idRubro: 1,  
-      empresa: {razonSocial: "este es el nombre de la empresa", localidad: "Chabas", direccion:"San Martin 1717", imagen: "https://fotografias.lasexta.com/clipping/cmsimages02/2019/11/14/66C024AF-E20B-49A5-8BC3-A21DD22B96E6/58.jpg"}, 
+      empresa: {razonSocial: "nombre de la empresa", localidad: "Chabas", direccion:"San Martin 1717", imagen: "https://fotografias.lasexta.com/clipping/cmsimages02/2019/11/14/66C024AF-E20B-49A5-8BC3-A21DD22B96E6/58.jpg"}, 
       nombre: "Notebook", imagen: "https://http2.mlstatic.com/notebook-lenovo-i3-8130u-4gb-1tb-156-pulgadas-dvdrw-D_NQ_NP_872956-MLA42418883269_062020-F.webp", precio: 50000,  descripcion: "Es una notebook, un producto muy bueno y de alta calidad.", stock: 25 };
   }
 
