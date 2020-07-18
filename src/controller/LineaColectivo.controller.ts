7384787139a302aa5abb23cbd5181c80f0a1de65
giuliano.truant <giulitruant_26@hotmail.com>
2020-07-18 11:24:40 -0300

    Try/Catch agregado en Linea de Colectivo

--- src/controller/LineaColectivo.controller.ts
+++ src/controller/LineaColectivo.controller.ts
@@ -3,45 +3,68 @@ import { getRepository, createQueryBuilder } from 'typeorm'
 import { LineaColectivo } from '../entity/LineaColectivo'
 
 export const getLineaColectivos = async (req: Request, res: Response): Promise<Response> => {
-    const lineasCol = await getRepository(LineaColectivo).find();
-    return res.json(lineasCol);
+    try {
+        const lineasCol = await getRepository(LineaColectivo).find();
+        if(lineasCol && lineasCol !== undefined){
+            return res.status(200).json(lineasCol);
+        } else{
+            return res.status(400).json({ Message: 'Linea de colectivo not found' });
+        }        
 
+    } catch (error) {
+        return res.status(400).json({ Message: error.message });
+    }
 }
 
-export const getLineaColectivo = async (req: Request, res: Response): Promise<Response> => {  
-    debugger;  
-    const lineaCol = await getRepository(LineaColectivo).findOne(req.params.id);
-    const lineaColEmpresa = await createQueryBuilder("LineaColectivo")    
-    .leftJoinAndSelect("LineaColectivo.empresa", "Empresa")
-    .where("LineaColectivo.idLineaColectivo = :idLineaColectivo", {idLineaColectivo : req.params.id})
-    .getOne();
-    return res.json(lineaColEmpresa);
+export const getLineaColectivo = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        // const lineaCol = await getRepository(LineaColectivo).findOne(req.params.id);
+        const lineaColEmpresa = await createQueryBuilder("LineaColectivo")
+        .leftJoinAndSelect("LineaColectivo.empresa", "Empresa")
+        .where("LineaColectivo.idLineaColectivo = :idLineaColectivo", {idLineaColectivo : req.params.id})
+        .getOne();
 
+    if(lineaColEmpresa != undefined && lineaColEmpresa){
+        return res.status(200).json(lineaColEmpresa);
+        
+    }else{
+        return res.status(400).json({ Message: 'Linea de colectivo not found' });
+    }
+
+    } catch (error) {
+        return res.status(400).json({ Message: error.message});
+    }
 }
 
-export const createLineaColectivo = async (req: Request, res: Response): Promise<Response> => {
-    debugger;
-    const lineaCol = await getRepository(LineaColectivo).create(req.body);
-    const result = await getRepository(LineaColectivo).save(lineaCol);
-    return res.json(result);
+export const createLineaColectivo = async (req: Request, res: Response): Promise<Response> => {    
+    try {
+        const lineaCol = await getRepository(LineaColectivo).create(req.body);
+        const result = await getRepository(LineaColectivo).save(lineaCol);
+        
+        return res.status(200).json(result);
+    
+    } catch (error) {
+        res.status(404).json({ message : error.message});
 
+    }
+    return res.status(400).json({ Message: 'Error al crear la linea de colectivo' });    
 }
 
 export const updateLineaColectivo = async (req: Request, res: Response): Promise<Response> => {    
-    try {        
+    try {
         const lineaCol = await getRepository(LineaColectivo).findOne(req.params.idLineaColectivo);    
         if(lineaCol !== undefined && lineaCol) {
             getRepository(LineaColectivo).merge(lineaCol, req.body);
             const result = await getRepository(LineaColectivo).save(lineaCol);
             return res.json(result);
+        } else{
+            return res.status(404).send({ message: 'Linea Colectivo not found' });
         }
     
-    return res.status(404).send({ message: 'Linea Colectivo not found' });
     } catch (error) {
-        return res.status(404).send({ message: error });
+        return res.status(404).send({ message: error.message });
 
-    }   
-
+    }
 }
 
 export const deleteLineaColectivo = async (req: Request, res: Response): Promise<Response> => {
@@ -53,6 +76,4 @@ export const deleteLineaColectivo = async (req: Request, res: Response): Promise
         return res.status(200).json({ msj: error });
 
     }
-    
-
 }
