47eb6d484ec48c1b9583801252db192f1a642ba2
Danilo Bassi <danilo-bassi@hotmail.com>
2020-07-31 23:12:56 -0300

    Agregado de base de datos en la nube

--- TP/Backend/server/database.js
+++ TP/Backend/server/database.js
@@ -1,10 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-// direccion de la base de datos.
-const URI = 'mongodb://localhost/online-shopping'
+// ------------------------------------------------- direccion de la base de datos ------------------------------------------------- //
+// para la base de datos en la nube usar esta.
+const user = 'ttads';
+const password = '1234';
+const database = 'ttads-tp'
+const URI = `mongodb+srv://${user}:${password}@ttads-tp.tyatw.gcp.mongodb.net/${database}?retryWrites=true&w=majority`;
 
-mongoose.connect(URI)
-    .then(db => console.log("Conectado correctamente a la DB."))
-    .catch(error => console.log(error))
+// para la base de datos local usar esta:
+//const URI = "mongodb://localhost/online-shopping";
 
-module.exports = mongoose
+mongoose
+  .connect(URI)
+  .then((db) => console.log("Conectado correctamente a la Base de datos."))
+  .catch((error) => console.log(error));
+
+module.exports = mongoose;
