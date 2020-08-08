eecb6cc6a901b9fe69204b9db0c3a355251cfb6f
Evelyn Hernandez <evelyn.hernandez@ecloudsolutions.com>
2020-08-08 01:55:00 -0300

    add gitignore. remove node modules folder from git

--- server/controllers/note.controller.js
+++ server/controllers/note.controller.js
@@ -4,42 +4,61 @@ const NoteCtrl = {}; //Creo el objeto controlador
 
 //Metodo GetAll (res= response y req= request)
 NoteCtrl.getNotes = async (req, res) => {
-    const notes = await Note.find(); //Busca todos los documentos
-    console.log(notes);
-    res.json(notes); //Los envio en formato JSON
+    try{ const notes = await Note.find(); //Busca todos los documentos
+        res.json(notes); //Los envio en formato JSON
+    } catch (err) {
+        res.status(500).send({ error: "Algo salió mal." });
+    }
 }
 
 //Metodo Create
 NoteCtrl.createNote = async (req, res) => {
-    const note = new Note({ //Creo la nueva nota con los parametros enviados en el request (sin ID porque lo da la BD)
-        name: req.body.name
-    });
-    await note.save(); //Guardo en la BD (y espero que finalice)
-    res.json({status: 'Nota Guardada Correctamente'}) //Devuelvo resultado correcto
+    try {
+        const note = new Note({ //Creo la nueva nota con los parametros enviados en el request (sin ID porque lo da la BD)
+            name: req.body.name
+        });
+        await note.save(); //Guardo en la BD (y espero que finalice)
+        res.json({status: 'Nota Guardada Correctamente'}) //Devuelvo resultado correcto
+    } catch(err){
+        console.log('error catch', err);
+        res.json({status: err})
+    }
 }
 
 //Metodo GetOne
 NoteCtrl.getNote = async (req, res) => {
-    const {id} = req.params; //Consigo el ID mando por parametro en el get
-    const note = await Note.findById(id); //Busco por ID
-    res.json(note); //Lo envío
+    try {
+        const {id} = req.params; //Consigo el ID mando por parametro en el get
+        const note = await Note.findById(id); //Busco por ID
+        res.json(note); //Lo envío
+    } catch (err) {
+        res.status(500).send({ error: "Entidad no encontrada." });
+    }
 }
 
 //Metodo Update
 NoteCtrl.updateNote = async (req, res) => {
-    const {id} = req.params;
-    const newNote = {
-        name: req.body.name
+    try {
+        const {id} = req.params;
+        const newNote = {
+            name: req.body.name
+        }
+        await Note.findByIdAndUpdate(id, {$set: newNote});
+        res.json({status: 'Nota Actualizada Correctamente'});
+    } catch(err){
+        res.json({status: err})
     }
-    await Note.findByIdAndUpdate(id, {$set: newNote});
-    res.json({status: 'Nota Actualizada Correctamente'});
 }
 
 //Metodo Delete
 NoteCtrl.deleteNote = async (req, res) => {
-    const {id} = req.params;
-    await Note.findByIdAndRemove(id);
-    res.json({status: 'Nota Eliminada Correctamente'});
+    try {
+        const {id} = req.params;
+        await Note.findByIdAndRemove(id);
+        res.json({status: 'Nota Eliminada Correctamente'});
+    } catch(err){
+        res.json({status: err})
+    }
 }
 
 //Exporto el controlador para requerirlo en otro lado
