db6b2654abf7df262c348745b11071da59925f4c
Agustin <agus.delmonti@gmail.com>
2020-11-26 16:36:39 -0300

    Order line states definition

--- api/resolvers/orderLine.js
+++ api/resolvers/orderLine.js
@@ -4,6 +4,13 @@ export default {
 			db.item.findOne({ where: { id: itemId } }),
 		order: ({ orderId }, args, { db }) =>
 			db.order.findOne({ where: { id: orderId } }),
+		status: (parent) => {
+			if (parent.canceledAt) return 'CANCELED';
+			if (parent.deliveredAt) return 'DELIVERED';
+			if (parent.finishedAt) return 'DONE';
+			if (parent.startedAt) return 'PROCESSING';
+			return 'PENDING';
+		},
 	},
 	Query: {
 		lines: (parent, args, { db }) => db.line.findAll(),
