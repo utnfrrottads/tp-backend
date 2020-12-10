2a7063427920fefecf559a966dbf9ce63c397669
Danilo Bassi <danilo-bassi@hotmail.com>
2020-12-10 15:10:42 -0300

    Arreglado bug en ventas

--- TP/Backend/server/models/ventas.js
+++ TP/Backend/server/models/ventas.js
@@ -1,16 +1,16 @@
-const mongoose = require ('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const VentaSchema = new Schema({
-    idComprador: { type: Schema.Types.ObjectId, ref: 'User', required:true },
-    productos: [{
-        producto: { type: Object, required: true },          
-        cantidad: { type: Number }
-      }],
-    fecha: { type: Date, required: false},
-    comisionista: { type: Object, required: false },
-    
-})
+  idComprador: { type: String, required: true },
+  productos: [
+    {
+      producto: { type: Object, required: true },
+      cantidad: { type: Number },
+    },
+  ],
+  fecha: { type: Date, required: false },
+  comisionista: { type: Object, required: false },
+});
 
-module.exports = mongoose.model('Venta', VentaSchema);
-
+module.exports = mongoose.model("Venta", VentaSchema);
