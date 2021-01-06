a1ba5eae6145e279c37a6fd2867925b4597297aa
Rinava <asdasd5056@gmail.com>
2021-01-06 17:58:59 -0300

    Minor Changes

--- Backend/models/cama.js
+++ Backend/models/cama.js
@@ -1,11 +1,12 @@
 module.exports = (sequelize, DataTypes) => {
     const Cama = sequelize.define("Cama", {
       id: {
+        primaryKey:true,
         type: DataTypes.INTEGER,
         allowNull: false
       },
       estado:{
-          type: DataTypes.BINARY,
+          type: DataTypes.BOOLEAN,
           allowNull: false
       }
     });
--- Backend/models/index.js
+++ Backend/models/index.js
@@ -8,6 +8,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
+
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
@@ -21,7 +22,7 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
     db[model.name] = model;
   });
 
