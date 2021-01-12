32618ab2c9f8f3a1792ad6e60b108512f13552ac
Lara Mateo <49590400+Rinava@users.noreply.github.com>
2021-01-12 20:45:40 -0300

    añadido persons a public_api

--- Backend/functions/src/public_api.ts
+++ Backend/functions/src/public_api.ts
@@ -1,3 +1,4 @@
 export { beds } from './routes/bed.routes';
 export { hospitals } from './routes/hospital.routes';
 export { healthInsurances } from './routes/healthInsurance.routes';
+export { persons } from './routes/person.routes';
