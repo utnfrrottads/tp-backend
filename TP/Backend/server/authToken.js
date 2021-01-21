470041b73d0afe989f603aba912732e6e092079d
Danilo Bassi <danilo-bassi@hotmail.com>
2021-01-21 20:33:52 -0300

    added pass encryption on the database

--- TP/Backend/server/authToken.js
+++ TP/Backend/server/authToken.js
@@ -3,13 +3,12 @@ const env = require("node-env-file");
 env(__dirname + "/.env.dist");
 
 module.exports = (req, res, next) => {
-
-  if (req.path.startsWith("/api/usuarios/") && req.method == 'PUT') {
+  if (req.path.startsWith("/api/usuarios/") && req.method == "PUT") {
     if (req.headers.authorization) {
       // verifico que el token sea valido
       let token = req.headers.authorization.split(" ")[1];
       jwt.verify(token, process.env.AUTH_SECRET, (error) => {
-        if (error) return res.status(500).send({ status: "Error",error });
+        if (error) return res.status(500).send({ status: "Error", error });
         next();
       });
     } else {
