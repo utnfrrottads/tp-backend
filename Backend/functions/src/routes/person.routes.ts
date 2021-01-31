7e656a22187a5cb895ab8df396a5ad9e909d54d8
Lucio Bevilacqua <fanacdc2@gmail.com>
2021-01-31 00:36:10 -0300

    ADD: create: emergencyContact and nurse, put: addEmergencyContactById

--- Backend/functions/src/routes/person.routes.ts
+++ Backend/functions/src/routes/person.routes.ts
@@ -34,7 +34,34 @@ person.post('/createPerson', [
     check('phone').not().isEmpty().withMessage('El campo phone es requerido'),
     sanitizeBody(['dni', 'firstName', 'lastName', 'bornDate', 'gender', 'phone', 'bloodType', 'emergencyContact', 'nurseWorkId', 'user', 'password']).trim(),
 ], PersonsController.createPerson);
-
+/**
+* `CREATES` a EmergencyContact.
+*/
+person.post('/createEmergencyContact', [
+    check('dni').not().isEmpty().withMessage('El campo dni es requerido'),
+    check('firstName').not().isEmpty().withMessage('El campo firstName es requerido'),
+    check('lastName').not().isEmpty().withMessage('El campo lastName es requerido'),
+    check('bornDate').not().isEmpty().withMessage('El campo bornDate es requerido'),
+    check('gender').not().isEmpty().withMessage('El campo gender es requerido'),
+    check('phone').not().isEmpty().withMessage('El campo phone es requerido'),
+    check('bloodType').not().isEmpty().withMessage('El campo bloodType es requerido'),
+    sanitizeBody(['dni', 'firstName', 'lastName', 'bornDate', 'gender', 'phone', 'bloodType', 'emergencyContact', 'nurseWorkId', 'user', 'password']).trim(),
+], PersonsController.createEmergencyContact);
+/**
+* `CREATES` a nurse.
+*/
+person.post('/createNurse', [
+    check('dni').not().isEmpty().withMessage('El campo dni es requerido'),
+    check('firstName').not().isEmpty().withMessage('El campo firstName es requerido'),
+    check('lastName').not().isEmpty().withMessage('El campo lastName es requerido'),
+    check('bornDate').not().isEmpty().withMessage('El campo bornDate es requerido'),
+    check('gender').not().isEmpty().withMessage('El campo gender es requerido'),
+    check('phone').not().isEmpty().withMessage('El campo phone es requerido'),
+    check('nurseWorkId').not().isEmpty().withMessage('El campo nurseWorkId es requerido'),
+    check('user').not().isEmpty().withMessage('El campo user es requerido'),
+    check('password').not().isEmpty().withMessage('El campo password es requerido'),
+    sanitizeBody(['dni', 'firstName', 'lastName', 'bornDate', 'gender', 'phone', 'bloodType', 'emergencyContact', 'nurseWorkId', 'user', 'password']).trim(),
+], PersonsController.createNurse);
 /**
 * `ADDS` a HealthInsurance that belongs to the person.
 */
@@ -56,6 +83,18 @@ person.put('/updatePersonById/:id', [
     param('id').isAlphanumeric().withMessage('El id debe ser alfanumérico'),
     sanitizeBody(['name', 'address', 'locality', 'phone', 'atentionLevel']).trim(),
 ], PersonsController.updatePersonById);
+
+/**
+* `ADD` an EmergencyContact by personId and contactId.
+*/
+person.put('/addEmergencyContactById/:personId/:contactId', [
+    param('personId').not().isEmpty().withMessage('El campo personId es requerido'),
+    param('personId').isLength({ min: 20, max: 20 }).withMessage('El personId debe tener 20 caracteres'),
+    param('personId').isAlphanumeric().withMessage('El personId debe ser alfanumérico'),
+    param('contactId').not().isEmpty().withMessage('El campo contactId es requerido'),
+    param('contactId').isLength({ min: 20, max: 20 }).withMessage('El contactId debe tener 20 caracteres'),
+    param('contactId').isAlphanumeric().withMessage('El contactId debe ser alfanumérico'),
+], PersonsController.addEmergencyContactById);
 
 /**
 * `DELETES` a person by ID.
