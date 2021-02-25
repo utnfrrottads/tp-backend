acdc202e9e2656abf5b244216de9cf54adf9cca0
Ornela Cosetti <orcorsetti@gmail.com>
2021-02-25 00:46:51 -0300

    Login without messages

--- frontend-ecommerce/src/app/Components/login/login.component.ts
+++ frontend-ecommerce/src/app/Components/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import {UserService} from '../../Services/user.service'
 
+declare var $: any;
+  
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,16 +12,34 @@ import {UserService} from '../../Services/user.service'
 })
 export class LoginComponent implements OnInit {
 
-  // username: string;
-  // password: string;
+  message = '';
 
   constructor(private userService: UserService) { }
-
+  
   ngOnInit(): void {
   }
+  
+  loginUser(username: string, password: string, form: NgForm){
+      try{
 
-  // loginUser(form: NgForm){
-  //     this.userService.loginUser(, form.value)
-  // }
+        if(username == null || password == null) {
+          this.message= "Ingrese todos los datos necesarios."
+          throw this.message
+        }
+        
+        this.userService.loginUser(username, password).subscribe(res => {
+          console.log(res)
+        }, 
+        (err) => {
+          if( JSON.stringify(err).includes("error")){
+            form.reset();
+            this.message = JSON.parse(JSON.stringify(err)).error.error
+            $('#alert').slideDown()
+          }
+        })
+      } catch {
+        $('#alert').show()
+      }
+  }
 
 }
