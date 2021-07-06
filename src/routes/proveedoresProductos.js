a38a419c406ea49c3711ad1cd31040b28f38e9a7
pablo-nardi <nardipablo31@gmail.com>
2021-07-06 19:52:42 -0300

    Agregando ruta prov_productos

--- src/routes/proveedoresProductos.js
+++ src/routes/proveedoresProductos.js
@@ -2,6 +2,18 @@ module.exports = app =>{
     const Proveedores = app.db.models.Proveedores;
     const Productos = app.db.models.Productos;
 
-    
+    app.route('/propro')
+        .get((req,res)=>{
+            Productos.findAll({
+                include:{
+                    model: Proveedores,
+                    required: true
+                }
+            })
+            .then(result=> res.json(result))
+            .catch(error=>{
+                res.status(412).json({msg: error.message});
+            });
+        });
 
 }
