fb330a91a43cc68a1bb33ccceaa66266f6959bd6
Gianfranco Raselli <66041020+GianfrancoRaselli@users.noreply.github.com>
2021-07-11 19:08:03 -0300

    swal cambiar clave

--- client/src/app/components/cambiar-clave/cambiar-clave.component.ts
+++ client/src/app/components/cambiar-clave/cambiar-clave.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 import { UserService } from '../../services/user.service';
 
@@ -32,6 +33,14 @@ export class CambiarClaveComponent implements OnInit {
         localStorage.setItem('token', res.data.cambiarClave.token);
 
         $("#btnCloseCambiarClavePopup").click();
+
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "Clave actualizada correctamente",
+          showConfirmButton: true,
+          timer: 4000
+        })
       },
       (err: any) => {
         this.errorMessage = err.message;
