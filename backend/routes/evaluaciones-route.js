9d7ff54aa6104b4d74484f3c3f679dec06075d6c
Lucas <lcordoba844@gmail.com>
2021-09-16 16:43:24 -0300

    Cargadas rutas evaluaciones

---
+++ backend/routes/evaluaciones-route.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+
+const controller = require('../controllers/evaluaciones-controller');
+
+router.get('/', async (req, res) => {
+    try {
+        let evaluaciones = await controller.getEvaluaciones();
+        res.status(200).json(evaluaciones);
+    } catch (error) {
+        res.status(400).json(error.message);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        let evaluacion = await controller.getEvaluacion(req.params.id);
+        res.status(200).json(evaluaciones);
+    } catch (error) {
+        res.status(400).json(error.message);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        let evaluacion = await controller.postEvaluacion(req.body);
+        res.status(201).json(empresa);
+    } catch (error) {
+        res.status(400).json(error.meesage);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        let evaluacion = await controller.putEvaluacion(req.params.id, req.body);
+        res.status(200).json(empresa);
+    } catch (error) {
+        res.status(400).json(error.message);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        await controller.deleteEvaluacion(req.params.id);
+        res.status(204).send();
+    } catch (error) {
+        res.status(400).json(error.message);
+    }
+});
+
+module.exports = router;
