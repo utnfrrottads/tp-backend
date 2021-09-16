fdb79b535cb94eebe2e66e678c962f3769fd4e4f
IgnacioCabanellas <ignaciomcabanellas@gmail.com>
2021-09-16 20:42:24 -0300

    Agregado de ABM entrevistas

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ const evaluadoresRoute = require('./routes/evaluadores-route');
 const candidatosRoute = require('./routes/candidatos-route');
 const vacantesRoute = require('./routes/vacantes-route');
 const evaluacionesRoute = require('./routes/evaluaciones-route');
+const entrevistasRoute = require('./routes/entrevistas-route');
 
 // Middleware.
 app.use(express.json());
@@ -24,6 +25,8 @@ app.use('/candidatos', candidatosRoute);
 app.use('/vacantes', vacantesRoute); 
 
 app.use('/evaluaciones', evaluacionesRoute);
+
+app.use('/entrevistas', entrevistasRoute);
 
 
 app.listen(port, () => {
