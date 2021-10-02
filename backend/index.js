c6b82a0b48685502df9ec08c93c7dc53fd1a2fa1
Francisco <49844500+Gexygamma@users.noreply.github.com>
2021-10-02 16:45:21 -0300

    Correcciones menores en index

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ const errorHandler = require('./middleware/error-handler');
 const app = express();
 const port = process.env.PORT || 8080;
 
-
 require ('colors');
 
 // Importamos las rutas de las entidades.
@@ -12,24 +11,20 @@ const candidatosRoute = require('./routes/candidatos-route');
 const vacantesRoute = require('./routes/vacantes-route');
 const evaluacionesRoute = require('./routes/evaluaciones-route');
 const entrevistasRoute = require('./routes/entrevistas-route');
+const empresasRoute = require('./routes/empresas-route');
 
 // Middleware.
 app.use(express.json());
 
-
 // Agregamos los endpoints a la API.
-
 app.use('/evaluadores', evaluadoresRoute);
-
 app.use('/candidatos', candidatosRoute); 
-
 app.use('/vacantes', vacantesRoute); 
-
 app.use('/evaluaciones', evaluacionesRoute);
-
 app.use('/entrevistas', entrevistasRoute);
+app.use('/empresas', empresasRoute);
 
-
+// Manejo de errores.
 app.use(errorHandler);
 
 app.listen(port, () => {
