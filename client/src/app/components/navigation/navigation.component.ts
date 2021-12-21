f211cd91ece21f5708e0c9d9dee2d160b052ffcf
Agustín D'Angelo <dangeloagustinariel@gmail.com>
2021-12-21 16:42:26 -0300

    removiendo jQuery de los modales

--- client/src/app/components/navigation/navigation.component.ts
+++ client/src/app/components/navigation/navigation.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef, ModalOptions } from 'ngx-bootstrap/modal';
 import { AuthService } from '../../services/auth.service';
+import { SignupComponent } from '../login/signup/signup.component';
+import { SigninComponent } from '../login/signin/signin.component';
 
 @Component({
   selector: 'app-navigation',
@@ -10,14 +12,23 @@ import { AuthService } from '../../services/auth.service';
 export class NavigationComponent implements OnInit {
 
   localStorage: Storage = localStorage;
+  bsModalRef?: BsModalRef;
 
-  constructor(public authService: AuthService) { }
+  constructor(
+    public authService: AuthService,
+    private modalService: BsModalService
+  ) { }
 
   ngOnInit(): void {
   }
 
-  cerrarNavMobile() {
-    $(".navbar-collapse").removeClass("show");
+  openSigninModal(): void {
+    this.bsModalRef = this.modalService.show(SigninComponent);
+    this.bsModalRef.content.closeBtnName = 'Close';
   }
 
+  openSignupModal(): void {
+    this.bsModalRef = this.modalService.show(SignupComponent);
+    this.bsModalRef.content.closeBtnName = 'Close';
+  }
 }
