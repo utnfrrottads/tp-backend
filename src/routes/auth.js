ff7034e7d1c798beb543284ff5e8fea754f9ff46
pablo-nardi <nardipablo31@gmail.com>
2022-01-12 18:55:14 -0300

    Retomando TP

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -8,12 +8,7 @@ module.exports = app =>{
     let secret = 'some_secret'; // a secret key is set here
     const Usuario = app.db.models.Usuarios;
     const bcrypt = require('bcrypt');
-    let actualUser = {
-        id: '',
-        usuario:'',
-        rol:'',
-        activo:''
-    };
+
 
     app.use(cors());
     app.options('*', cors());
@@ -28,37 +23,41 @@ module.exports = app =>{
 
     app.route('/api/login')
         .post((req,res)=>{
-            Usuario.findOne({where: {usuario: req.body.usuario, activo: true} })
+            let actualUser = {
+                id: '',
+                usuario:'',
+                rol:'',
+                activo:''
+            };
+            Usuario.findOne({where: {usuario: req.body.usuario  } })
                 .then(user =>{
                     actualUser.id = user.id;
                     actualUser.usuario = user.usuario;
                     actualUser.rol = user.rol;
                     actualUser.activo = user.activo;
-                    bcrypt.compare(req.body.clave, user.clave)
-                        .then(result =>{
-                            if(result){
+                    if(actualUser.activo){
+                        bcrypt.compare(req.body.clave, user.clave)
+                            .then(result =>{
+                                if(result){
                                     let token = jwt.sign(actualUser, secret, { expiresIn: '7200s'});
                                     let loginUser = {
                                         user: actualUser,
                                         token: token
                                     }
                                     res.send(loginUser);
-                            }else{
-                                res.status(401).json({status: 401, mensaje: 'usuario y/o contraseña incorectos'});
-                            }
-                        })
-                        .catch(err =>{
-                            res.status(500).json({status: 500, mensaje: 'error al validar la contraseña'});
-                        });
+                                }else{
+                                    res.status(401).json({status: 401, mensaje: 'usuario y/o contraseña incorectos'});
+                                }
+                            })
+                            .catch(err =>{
+                                res.status(500).json({status: 500, mensaje: 'error interno del servidor'});
+                            });
+                    }else{
+                        res.status(403).json({status: 403, mensaje: 'usuario inactivo'});
+                    }
                 })
                 .catch(error =>{
-                    console.log('valor de usuario',actualUser.activo )
-                    if(actualUser.activo){
-                        res.status(403).json({status: 403, mensaje: 'usuario inactivo'});
-                    }else{
-                        res.status(403).json({status: 403, mensaje: 'usuario y/o contraseña incorectos'});
-                    }
-
+                    res.status(403).json({status: 403, mensaje: 'usuario y/o contraseña incorectos'});
                 })
 
         })
