44363401e380b2555e49d2b2d4317e84b7874ba1
andresbertone <andresbertone97@gmail.com>
2022-04-14 11:48:47 -0300

    Fix: Uso de errorHandler

--- backend/routes/evaluadores-route.js
+++ backend/routes/evaluadores-route.js
@@ -4,46 +4,46 @@ const evaluadoresController = require('../controllers/evaluadores-controller');
 
 
 // Crear un nuevo evaluador.
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
     try {
         await personasController.createPerson(req.body, 'evaluador');
         res.status(200).json('Evaluator created successfully');
     } catch (error) {
-        res.status(400).json( error.message );
+        next(error);
     }
 });
 
 
 // Modificar los datos de un evaluador.
-router.put('/:id_persona', async (req, res) => {
+router.put('/:id_persona', async (req, res, next) => {
     try {
         await personasController.updatePerson(req.params.id_persona, req.body, 'evaluador');
         // FIXME: Ver de estandarizar lo que se devuelve al frontend. O mostramos mensaje, o enviamos el objeto actualizado o ambas cosas. (VER EN LOS OTROS ROUTES)
         res.status(200).json('Evaluator updated successfully');
     } catch (error) {
-        res.status(400).json( error.message );
+        next(error);
     }
 });
 
 
 // Eliminar un evaluador.
-router.delete('/:id_persona', async (req, res) => {
+router.delete('/:id_persona', async (req, res, next) => {
     try {
         await personasController.deletePerson(req.params.id_persona, 'evaluador');
         res.status(200).json('Evaluator deleted successfully');
     } catch (error) {
-        res.status(400).json( error.message );
+        next(error);
     }
 });
 
 
 // Devuelve todos los evaluadores
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
     try {
         const evaluators = await evaluadoresController.getEvaluadores();
         res.status(200).json(evaluators);
     } catch (error) {
-        res.status(400).json( error.message );
+        next(error);
     }
 });
 
