37dc38863234b242836b9252254a3c5d989af205
Agustín DAngelo <dangeloagustinariel@gmail.com>
2022-04-29 23:11:36 -0300

    styling the code

--- server/src/graphql/schema.js
+++ server/src/graphql/schema.js
@@ -51,14 +51,55 @@ const {
 const QueryType = new GraphQLObjectType({
     name: "QueryType",
     description: "Queries",
-    fields: { usuario, niveles, categorias, getCategoriaById, monedas, servicio, servicios, misServicios, detalleServicio, contrato, serviciosContratados, estadisticasServicio, contratosRealizados, contratosRecibidos, mensajesDelContrato, notificacion, misNotificaciones, estadisticas }
+    fields: { 
+        usuario,
+        niveles,
+        categorias,
+        getCategoriaById,
+        monedas,
+        servicio,
+        servicios,
+        misServicios,
+        detalleServicio,
+        contrato,
+        serviciosContratados,
+        estadisticasServicio,
+        contratosRealizados,
+        contratosRecibidos,
+        mensajesDelContrato,
+        notificacion,
+        misNotificaciones,
+        estadisticas
+    }
 })
 
 // Define MutationType
 const MutationType = new GraphQLObjectType({
     name: "MutationType",
     description: "Mutations",
-    fields: { signUp, signIn, updateUsuario, cambiarClave, updateProfileImage, deleteProfileImage, addNivel, deleteNivel, updateNivel, addCategoria, deleteCategoria, updateCategoria, publishService, signContract, cancelContract, confirmContract, finishContract, setScore, readNotifications, openNotification, deleteAccount }
+    fields: { 
+        signUp,
+        signIn,
+        updateUsuario,
+        cambiarClave,
+        updateProfileImage,
+        deleteProfileImage,
+        addNivel,
+        deleteNivel,
+        updateNivel,
+        addCategoria,
+        deleteCategoria,
+        updateCategoria,
+        publishService,
+        signContract,
+        cancelContract,
+        confirmContract,
+        finishContract,
+        setScore,
+        readNotifications,
+        openNotification,
+        deleteAccount
+    }
 })
 
 module.exports = new GraphQLSchema({
