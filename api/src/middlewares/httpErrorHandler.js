62986a86560191d2b89e7facf69dbeb8eb24b25f
andresbertone <andresbertone97@gmail.com>
2022-09-23 11:48:45 -0300

    Fix: Manejador de errores

--- api/src/middlewares/httpErrorHandler.js
+++ api/src/middlewares/httpErrorHandler.js
@@ -2,22 +2,20 @@ const ApiError = require('../utils/apiError');
 
 
 const httpErrorHandler = (error, req, res, next) => {
-    const defaultStatusCode = 500;
-    const errorMessage = {
-        message: error.message
-    };
+    const defaultStatusCode = error.status || 500; // Si el error viene de validaciones setea la variable con el valor "error.status". Sino con 500.
+    const errorMessage = error.errors || [{ message: error.message }]; // Si el error viene de validaciones, setea la variable con el valor "error.errors". Sino con "[{ message: error.message }]".
 
     if ( error instanceof ApiError ) {
         return res.status(error.statusCode).json({
             status: error.statusCode,
-            errors: [errorMessage],
+            errors: errorMessage,
             data: {}
         });
     }
 
     return res.status(defaultStatusCode).json({
         status: defaultStatusCode,
-        errors: [errorMessage],
+        errors: errorMessage,
         data: {}
     });
 };
